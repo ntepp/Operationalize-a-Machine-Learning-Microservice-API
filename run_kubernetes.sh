d8d49b84b239a237b5b7b2b56439ddfcf72643a7
ntepp <ntepp.marcus@gmail.com>
2020-05-01 11:09:08 +0000

    Configure Kubernetes to Run Locally

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=jmntepp/nteppmlapi
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlmicroserviceapi --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=nteppmlmicroserviceapi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward nteppmlmicroserviceapi 8000:80
 
